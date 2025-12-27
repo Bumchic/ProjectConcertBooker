d4e366966a3d4ae421a2c92adb63573aef4ddf64
Minh <duonganhminh05@gmail.com>
2025-12-27 17:49:42 +0700

    removed seat for compile error

--- lib/Seat.dart
+++ lib/Seat.dart
@@ -1,11 +1,8 @@
 class Seat {
   int seatvertivalamount = 0;
   int seathorizontalamount = 0;
-  List<List<int>> seats;
-  Seat({required this.seathorizontalamount, required this.seatvertivalamount})
-  {
-    seats = List.generate(seathorizontalamount, (i) => List.generate(seatvertivalamount, (j) => 1));
-  }
+
+  Seat({required this.seathorizontalamount, required this.seatvertivalamount});
   int GetSeatAmount()
   {
     return seatvertivalamount * seathorizontalamount;
